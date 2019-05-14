d562cf8ec352e2f634a8654d77a41ec1ced053f1
norrilsk <norrilsk@gmail.com>
2019-05-14 16:56:43 +0300

    add StoreQueue

--- code/Config.hpp
+++ code/Config.hpp
@@ -16,14 +16,18 @@ private:
     Log::Loger log;
     Log::Loger trace;
     bool dump_trace = false;
+    bool enable_sq = false;
+    uint32_t sq_size = 0;
    
 public:
     uint64_t get_mem_size() const{ return memory_size;};
     uint64_t get_page_size()const{ return page_size;};
     uint64_t get_num_regs()const{return num_regs;};
     uint32_t get_num_of_pipeline_stages() const{return pipeline_stages;}
+    uint32_t get_sq_size() const{return sq_size;}
     bool get_log_marlin() const{return marlin_log;};
     bool is_dump_trace() const { return  dump_trace;}
+    bool is_sq_enabled() const {return enable_sq;}
     Log::Loger& get_log_ref() {return log;};
     Log::Loger&  get_trace_ref() {return trace;}
     void read_config_file(std::string path);
